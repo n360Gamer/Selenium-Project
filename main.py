a935617237f51421aa6a64914067ea3ae2b769af
Nathan La-Desktop <Nathantranla@gmail.com>
2025-04-26 23:43:58 -0700

    revamped the code so its more efficient and doesnt skip the offers, and simplifed it alot. Also made it so it will wait untill its located on the page, if not it will exit

--- main.py
+++ main.py
@@ -5,36 +5,34 @@ from selenium.webdriver.chrome.options import Options
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Set up the browser
 chrome_options = Options()
 chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 driver = webdriver.Chrome(options=chrome_options)
 
-# Open a website
-driver.get(
-    "https://secure.chase.com/web/auth/dashboard#/dashboard/merchantOffers/offerCategoriesPage?accountId=1034016530"
-)
+CHASE_URL = os.getenv("CHASE_URL")
+index = 0
+try:
+    while True:
+        # Check if the "Add" button is present
+        driver.get(CHASE_URL)
+        time.sleep(1)  # Wait for the page to load
+        wait = WebDriverWait(driver, 5)
 
+        add_button = wait.until(
+            EC.presence_of_element_located((By.CLASS_NAME, "r9jbijb"))
+        )
+        add_button.click()
+        index += 1
+        print(f"Clicked on cashback offer {index}")
+        time.sleep(1)
 
-def add_cashback_item(index):
-    cash_back_boxes = driver.find_elements(By.CLASS_NAME, "r9jbija")
-    cash_back_boxes[index].click()
-    time.sleep(2)
-    driver.get(
-        "https://secure.chase.com/web/auth/dashboard#/dashboard/merchantOffers/offerCategoriesPage?accountId=1034016530"
-    )
-
-# Find an element (e.g., the search box)
-list_of_adding = driver.find_elements(By.CLASS_NAME, "r9jbija")
-
-number_of_boxes = len(list_of_adding)
-
-for i in range(number_of_boxes):
-    list_of_adding = driver.find_elements(By.CLASS_NAME, "r9jbija")
-    add_cashback_item(i)
-    time.sleep(1)
-
-time.sleep(5)
-driver.quit()
+except TimeoutException as e:
+    print("No more cashback offers to add.")
+    driver.quit()
